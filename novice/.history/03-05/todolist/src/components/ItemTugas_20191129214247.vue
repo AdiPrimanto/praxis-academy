9d0c6bd79e915f6d768eaece3093057f831540cd
adiprimanto <adiprimanto.98@gmail.com>
2019-11-29 22:08:07 +0700

    update 04-02

---
+++ novice/.history/03-05/todolist/src/components/ItemTugas_20191129214247.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input class="form-check-input" onclick="" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label item" v-bind:class="{'item-completed':tugasSaya.isDone}" for="defaultCheck1">
+            {{tugasSaya.nama}}
+        </label>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 
+    }
+</script>
+
+<style scoped>
+
+</style>
