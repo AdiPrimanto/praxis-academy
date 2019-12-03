7be02e03d611b4f9e2844e5b479e3a8c7625cf50
adiprimanto <adiprimanto.98@gmail.com>
2019-12-03 10:09:01 +0700

    update kasus

---
+++ novice/.history/04-04/latihan/spa/src/Main_20191202162509.js
@@ -0,0 +1,28 @@
+index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+import "./style.css";
+ 
+ReactDOM.render(
+  <Main/>, 
+  document.getElementById("root")
+);
+1
+2
+3
+4
+5
+6
+7
+8
+9
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+import "./style.css";
+ 
+ReactDOM.render(
+  <Main/>, 
+  document.getElementById("root")
+);
---
+++ novice/.history/04-04/latihan/spa/src/index_20191202162422.js
@@ -0,0 +1,10 @@
+index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+import "./style.css";
+ 
+ReactDOM.render(
+  <Main/>, 
+  document.getElementById("root")
+);
